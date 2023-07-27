98a68012d5a066d6ce019d60ebf3c069033f6498
tamo-tmry <mollianium@gmail.com>
2023-07-28 01:21:23 +0900

    test: 環境変数のデバッグ

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test'
 require('dotenv').config()
 
-const appUrl = process.env.APP_URL!
+const appUrl = process.env.APP_URL || 'http://127.0.0.1:3000/'
+
+console.log('DEBUG appUrl: ', appUrl)
 
 test('has link', async ({ page }) => {
   await page.goto(appUrl)
