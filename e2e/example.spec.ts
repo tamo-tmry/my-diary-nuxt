debaa07e4bcb4093eb61313c424d3ab16456f2b0
tamo-tmry <mollianium@gmail.com>
2023-07-28 02:15:38 +0900

    test: e2eの設定調整

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,16 +1,14 @@
 import { test, expect } from '@playwright/test'
 require('dotenv').config()
 
-const appUrl = process.env.APP_URL!
-
 test('has link', async ({ page }) => {
-  await page.goto(appUrl)
+  await page.goto('/')
   const registerElement = page.getByRole('link', { name: '新規登録' })
   await expect(registerElement).toBeVisible()
 })
 
 test('vrt test', async ({ page }) => {
-  await page.goto(appUrl)
+  await page.goto('/')
   const registerElement = page.getByRole('link', { name: '新規登録' })
   await expect(registerElement).toBeVisible()
   await expect(page).toHaveScreenshot()
