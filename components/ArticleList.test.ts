1f7d8f7b7b27dec158f793b8cb5a9a71ded478cb
tamo-tmry <mollianium@gmail.com>
2023-08-14 02:10:34 +0900

    test: 💍 一覧ページにて投稿がない場合の表示とテストを追加

--- components/ArticleList.test.ts
+++ components/ArticleList.test.ts
@@ -9,31 +9,81 @@ Vue.use(VueRouter) // VueにVueRouterをインストールします
 
 const router = new VueRouter() // 新しいRouterインスタンスを作成します
 
-test('', () => {
+test('一覧表示される', () => {
   const vuetify = new Vuetify()
+
+  const articles = [
+    {
+      id: 'article-id-1',
+      title: 'article-title-1',
+      content: 'article-content-1',
+    },
+    {
+      id: 'article-id-2',
+      title: 'article-title-2',
+      content: 'article-content-2',
+    },
+    {
+      id: 'article-id-3',
+      title: 'article-title-3',
+      content: 'article-content-3',
+    },
+  ]
 
   render(ArticleList, {
     props: {
-      articles: [
-        {
-          id: 'article-id-1',
-          title: 'article-title-1',
-          content: 'article-content-1',
-        },
-        {
-          id: 'article-id-2',
-          title: 'article-title-2',
-          content: 'article-content-2',
-        },
-        {
-          id: 'article-id-3',
-          title: 'article-title-3',
-          content: 'article-content-3',
-        },
-      ],
+      articles,
     },
     vuetify,
     routes: router,
   })
-  expect(screen.getByText('article-title-1')).toBeInTheDocument()
+  const list = screen.getByRole('list')
+  expect(list).toBeInTheDocument()
+})
+
+test('articlesの数だけ一覧表示される', () => {
+  const vuetify = new Vuetify()
+
+  const articles = [
+    {
+      id: 'article-id-1',
+      title: 'article-title-1',
+      content: 'article-content-1',
+    },
+    {
+      id: 'article-id-2',
+      title: 'article-title-2',
+      content: 'article-content-2',
+    },
+    {
+      id: 'article-id-3',
+      title: 'article-title-3',
+      content: 'article-content-3',
+    },
+  ]
+
+  render(ArticleList, {
+    props: {
+      articles,
+    },
+    vuetify,
+    routes: router,
+  })
+  expect(screen.getAllByRole('listitem')).toHaveLength(articles.length)
+})
+
+test('投稿がない場合、「投稿がありません。」が表示される', () => {
+  const vuetify = new Vuetify()
+
+  render(ArticleList, {
+    props: {
+      articles: [],
+    },
+    vuetify,
+    routes: router,
+  })
+  const list = screen.queryByRole('list')
+  expect(list).not.toBeInTheDocument()
+  expect(list).toBeNull()
+  expect(screen.getByText('投稿がありません。')).toBeInTheDocument()
 })
--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -1,14 +1,13 @@
 <template>
-  <v-list>
-    <v-list-item
-      v-for="article in articles"
-      :key="article.id"
-      :to="`/article/${article.id}`"
-    >
-      <v-list-item-title>{{ article.title }}</v-list-item-title>
-      <v-list-item-subtitle>{{ article.content }}</v-list-item-subtitle>
-    </v-list-item>
-  </v-list>
+  <div>
+    <v-list v-if="hasArticle">
+      <v-list-item v-for="article in articles" :key="article.id" :to="`/article/${article.id}`">
+        <v-list-item-title>{{ article.title }}</v-list-item-title>
+        <v-list-item-subtitle>{{ article.content }}</v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+    <p v-else>投稿がありません。</p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -19,6 +18,11 @@ export default Vue.extend({
     articles: {
       type: Array as PropType<any[]>, // TODO: 型定義修正
       required: true,
+    },
+  },
+  computed: {
+    hasArticle() {
+      return Boolean(this.articles.length)
     },
   },
 })
