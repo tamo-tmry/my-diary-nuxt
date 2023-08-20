1440d150f6e502102d5597355ed39083c3639993
tamo-tmry <mollianium@gmail.com>
2023-08-20 23:45:18 +0900

    test: 💍 Viewport設定追加

--- components/ArticleList.stories.js
+++ components/ArticleList.stories.js
@@ -1,38 +1,69 @@
-import ArticleList from '~/components/ArticleList.vue'
+import ArticleList from './ArticleList.vue'
+import { SPStory } from '~/test/util'
 
 export default {
   title: 'ArticleList',
-  components: { ArticleList },
-  argTypes: {
-    articles: { control: 'object' },
+  component: ArticleList,
+}
+
+export const Default = {
+  args: {
+    articles: [
+      {
+        id: 'article-id-1',
+        title: 'article-title-1',
+        content: 'article-content-1',
+      },
+      {
+        id: 'article-id-2',
+        title: 'article-title-2',
+        content: 'article-content-2',
+      },
+      {
+        id: 'article-id-3',
+        title: 'article-title-3',
+        content: 'article-content-3',
+      },
+    ],
   },
 }
 
-const Template = (args) => ({
-  components: { ArticleList },
-  setup() {
-    return { args }
+export const NoList = {
+  args: {
+    articles: [],
   },
-  template: '<ArticleList v-bind="args" />',
-})
+}
 
-export const Default = Template.bind({})
-Default.args = {
-  articles: [
-    {
-      id: 'article-id-1',
-      title: 'article-title-1',
-      content: 'article-content-1',
-    },
-    {
-      id: 'article-id-2',
-      title: 'article-title-2',
-      content: 'article-content-2',
-    },
-    {
-      id: 'article-id-3',
-      title: 'article-title-3',
-      content: 'article-content-3',
-    },
-  ],
+export const DefaultBySP = {
+  args: {
+    articles: [
+      {
+        id: 'article-id-1',
+        title: 'article-title-1',
+        content: 'article-content-1',
+      },
+      {
+        id: 'article-id-2',
+        title: 'article-title-2',
+        content: 'article-content-2',
+      },
+      {
+        id: 'article-id-3',
+        title: 'article-title-3',
+        content: 'article-content-3',
+      },
+    ],
+  },
+  parameters: {
+    ...SPStory.parameters,
+  },
+}
+
+export const NoListBySP = {
+  args: {
+    articles: [],
+  },
+  parameters: {
+    ...SPStory.parameters,
+  },
 }
---
+++ test/util.ts
@@ -0,0 +1,18 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+export const SPStory = {
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphone6',
+    },
+    screenshot: {
+      viewport: {
+        width: 375,
+        height: 667,
+        deviceScaleFactor: 1,
+      },
+      fullPage: false,
+    },
+  },
+}
