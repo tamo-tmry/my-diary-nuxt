c1e8870001b68e2540c6b963779210fa6fde4e1e
tamo-tmry <mollianium@gmail.com>
2023-10-02 23:15:29 +0900

    feat: colorsのimport

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,6 +1,7 @@
 import { createVuetify, ThemeDefinition } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import colors from 'vuetify/lib/util/colors'
 
 export enum ThemeName {
   Light = 'light',
@@ -10,13 +11,13 @@ export enum ThemeName {
 const darkTheme: ThemeDefinition = {
   dark: true,
   colors: {
-    primary: '#2196F3',
-    secondary: '#424242',
-    accent: '#FF4081',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00'
+    primary: colors.blue.darken2,
+    accent: colors.grey.darken3,
+    secondary: colors.amber.darken3,
+    info: colors.teal.lighten1,
+    warning: colors.amber.base,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3,
   }
 }
 
